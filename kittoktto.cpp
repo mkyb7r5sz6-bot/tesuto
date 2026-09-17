aa9a677c35b3405519f5b643918c6e7aeccaedbc
mstys <sakuraikon014@icloud.com>
2026-09-17 14:20:33 +0900

    やったねへんこうできた　ラーメン食べたい

---
+++ kittoktto.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void Init(int array[])
+{
+	for (int i = 0;i < 5;++i)
+	{
+		array[i] = i * 5;
+	}
+
+}
+void show(int array[])
+{
+	for (int i = 0; i < 5; ++i)
+	{
+		cout << array[i] << ' ';
+	}
+	cout << endl;
+}
+
+int main()
+{
+	int n[5];
+
+	Init(n);
+	show(n);
+}
